f9d536fabf80ef87cdfd516ff2a93f84ba4cdcf4
DiachukRoma <roma19.05.94@gmail.com>
2025-10-04 22:42:25 +0300

    small changes

--- web/app/themes/k44/resources/views/404.blade.php
+++ web/app/themes/k44/resources/views/404.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="top" style="background-image: url('{{ $back_image->top_image['url'] }}')" width="{{ $back_image->top_image['width'] }}" height="{{ $back_image->top_image['height'] }}">
-    <div class="container">
-      <h1 class="top__title">404</h1>
-      <p class="top__subtitle">Сторінка не знайдена</p>
-    </div>
-  </section>
+<section class="top" style="background-image: url('{{ $back_image->top_image['url'] }}')"
+  width="{{ $back_image->top_image['width'] }}" height="{{ $back_image->top_image['height'] }}">
+  <div class="container">
+    <h1 class="top__title">404</h1>
+    <p class="top__subtitle">{{ __('Сторінка не знайдена') }}</p>
+  </div>
+</section>
 
-  <section class="content">
-    <div class="container">
-      <div class="alert alert-warning">
-        <h2>Вибачте, але сторінка, яку ви намагалися переглянути, не існує.</h2>
-      </div>
+<section class="content">
+  <div class="container">
+    <div class="alert alert-warning">
+      <h2>{{ __('Вибачте, але сторінка, яку ви намагалися переглянути, не існує.', 'k44') }}</h2>
     </div>
-  </section>
+  </div>
+</section>
 @endsection
